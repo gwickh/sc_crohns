af3339ed985b646ca6a82ca8cf93e07c95b52f0f
gwick <Greg.Wickham@earlham.ac.uk>
2025-05-20 16:52:32 +0100

    add script to perform feature selection and PCA

--- clustering/1_clustering.sh
+++ clustering/1_clustering.sh
@@ -2,8 +2,8 @@
 
 # SLURM directives
 #SBATCH --job-name=seurat_cluster
-#SBATCH --output=1_seurat_load_matrices_%j.SLURMout
-#SBATCH --error=1_seurat_load_matrices_%j.SLURMerr
+#SBATCH --output=1_seurat_load_matrices_%j.SLURM.stdout
+#SBATCH --error=1_seurat_load_matrices_%j.SLURM.stderr
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -21,8 +21,14 @@ echo "Create the Seurat object"
 source /hpc-home/yep25yan/mamba/etc/profile.d/conda.sh
 conda activate r_env
 
+# STEP 1A: Load matrices, create Seurat object, perform normalization, cell cycle scoring and scale by regression.
 Rscript "${SCRIPT_DIR}/1_seurat_load_matrices.R" \
-        1> "${OUT_DIR}/1_seurat_load_matrices.STDOUT" \
-        2> "${OUT_DIR}/1_seurat_load_matrices.STDERR"
+        1> "${OUT_DIR}/1_seurat_load_matrices.R.stdout" \
+        2> "${OUT_DIR}/1_seurat_load_matrices.R.stderr"
+
+# STEP 1B: Perform PCA and feature selection.
+# Rscript "${SCRIPT_DIR}/1b_seurat_PCA.R" \
+#         1> "${OUT_DIR}/1b_seurat_PCA.R.stdout" \
+#         2> "${OUT_DIR}/1b_seurat_PCA.R.stderr"
 
 exit 0
