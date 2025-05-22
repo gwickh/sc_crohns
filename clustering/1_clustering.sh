9d898cd0879c846393a4cd71b19c8a6c1bde1263
gwick <Greg.Wickham@earlham.ac.uk>
2025-05-22 16:00:27 +0100

    refactor to loop over scripts

--- clustering/1_clustering.sh
+++ clustering/1_clustering.sh
@@ -2,10 +2,10 @@
 
 # SLURM directives
 #SBATCH --job-name=seurat_cluster
-#SBATCH --output=1_seurat_load_matrices_%j.SLURM.stdout
-#SBATCH --error=1_seurat_load_matrices_%j.SLURM.stderr
+#SBATCH --output=1_clustering_%j.SLURM.stdout
+#SBATCH --error=1_clustering_%j.SLURM.stderr
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=64G
+#SBATCH --mem=128G
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=yep25yan@nbi.ac.uk
 
@@ -16,21 +16,22 @@ OUT_DIR="$2"
 # Create output directory if it doesn't exist
 mkdir -p "${OUT_DIR}"
 
-echo "Create the Seurat object"
-
 source /hpc-home/yep25yan/mamba/etc/profile.d/conda.sh
 conda activate r_env
+scripts=(
+    "1_seurat_load_matrices.R"
+    "1b_seurat_PCA.R"
+    "1c_seurat_clustering.R"
+    "1d_seurat_cluster_comp.R"
+    "1e_seurat_UMAP.R"
+)
 
-echo STEP 1A: Load matrices, create Seurat object, perform normalization, cell cycle scoring and scale by regression.
-Rscript "${SCRIPT_DIR}/1_seurat_load_matrices.R" \
-        1> "${OUT_DIR}/1_seurat_load_matrices.R.stdout" \
-        2> "${OUT_DIR}/1_seurat_load_matrices.R.stderr"
-echo "merged_object.Rds written to ${OUT_DIR}"
-
-# echo STEP 1B: Perform feature selection and PCA.
-# Rscript "${SCRIPT_DIR}/1b_seurat_PCA.R" \
-#         1> "${OUT_DIR}/1b_seurat_PCA.R.stdout" \
-#         2> "${OUT_DIR}/1b_seurat_PCA.R.stderr"
-# echo "dimensionally reduced merged_object.Rds written to ${OUT_DIR}"
+for script in "${scripts[@]}"; do
+    echo "Running ${script}"
+    Rscript "${SCRIPT_DIR}/${script}" \
+            1> "${OUT_DIR}/${script}.stdout" \
+            2> "${OUT_DIR}/${script}.stderr"
+    echo "${script} completed"
+done
 
 exit 0
