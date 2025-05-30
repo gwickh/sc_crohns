4a8aa940f30651129032e866f831f567d937cbb8
gwick <Greg.Wickham@earlham.ac.uk>
2025-05-30 16:20:41 +0100

    rename

---
+++ clustering/1_clustering_slurm.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# SLURM directives
+#SBATCH --job-name=seurat_cluster
+#SBATCH --output=1_clustering_%j.SLURM.stdout
+#SBATCH --error=1_clustering_%j.SLURM.stderr
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=128G
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=yep25yan@nbi.ac.uk
+
+# Arguments
+SCRIPT_DIR="$1"
+OUT_DIR="$2"
+
+# Create output directory if it doesn't exist
+mkdir -p "${OUT_DIR}"
+
+source /hpc-home/yep25yan/mamba/etc/profile.d/conda.sh
+conda activate r_env
+scripts=(
+    "1a_seurat_load_matrices.R"
+    "1b_seurat_PCA.R"
+    # "1c_seurat_clustering.R"
+    # "1d_seurat_cluster_comp.R"
+    # "1e_seurat_UMAP.R"
+)
+
+for script in "${scripts[@]}"; do
+    echo "Running ${script}"
+    Rscript "${SCRIPT_DIR}/${script}" \
+            1> "${OUT_DIR}/${script}.stdout" \
+            2> "${OUT_DIR}/${script}.stderr"
+    echo "${script} completed"
+done
+
+exit 0
