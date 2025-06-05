11b2178efd0d8c23a009529c6659d414aa1f3a7b
gwick <Greg.Wickham@earlham.ac.uk>
2025-06-05 16:44:42 +0100

    standardise filenames, use Seurat accessor functions instead of slot access

--- clustering/1c_seurat_clustering.R
+++ clustering/1c_seurat_clustering.R
@@ -1,5 +1,9 @@
 source(file.path("sc_crohns/clustering", ".Rprofile"))
 
+if (!exists("seurat_object")) {
+  seurat_object <- readRDS(file.path(SEURAT_OBJECT_PATH, "seurat_object.Rds"))
+}
+
 # Function to identify number of informative PCs --------------------------
 # based on standard deviation cutoff and cluster with Louvain at varying on
 # resolution and kNN
@@ -7,80 +11,87 @@ Clust_dr <- function(
     seurat_object, 
     dr, 
     pcs = 50, 
-    pval = 1e-05, 
     non.rand.sd.frac = 0.5, 
-    k, 
+    K, 
     res, 
     file
 ) {
   # Compute minimum std a PC must have to be considered “non-random” assuming
   # last 10 PCs approximate random noise to exclude SVD artifacts
-  min_stdev <- (1+non.rand.sd.frac)*mean(seurat_object@reductions[[dr]]@stdev[(pcs-10):pcs])
+  stdevs <- Stdev(seurat_object[[dr]])
+  last10_idx <- max(1, pcs - 9):pcs
+  mean_stdev_lastPCs <- mean(stdevs[last10_idx])
+  min_stdev <- (1 + non.rand.sd.frac) * mean_stdev_lastPCs
   
   # Loops through PCs to determine nPCs, the number of informative PCs that 
   # explains > 0.05 variance with respect to the next
-  for (nPCs in 0:(length(seurat_object@reductions[[dr]]@stdev)-1)) {
-    if (seurat_object@reductions[[dr]]@stdev[nPCs+1] < min_stdev) 
-      break
+  nPCs <- 0
+  for (i in seq_along(stdevs)) {
+    if (stdevs[i] < min_stdev) break
+    nPCs <- i
   }
   write(nPCs, file = file)
   
   # For each k build a NN graph using the top nPCs components
-  for (kk in k) {
+  for (k in K) {
     seurat_object <- FindNeighbors(
       seurat_object, 
-      k.param = kk, 
+      k.param = k, 
       reduction = dr, 
       dims = 1:nPCs
     )
     # For each resolution, run Louvain clustering
     for (r in res) {
-      cl_ident <- paste("clusters_", dr, "_k", kk, "_res", r, sep="")
+      cl_ident <- paste("clusters", dr, "k", k, "res", r, sep="_")
       seurat_object <- FindClusters(seurat_object, resolution = r)
-      names(seurat_object@meta.data)[names(seurat_object@meta.data) == "seurat_clusters"] <- cl_ident
-      seurat_object@active.ident <- as.factor(seurat_object$orig.ident)
+      seurat_object[[cl_ident]] <- Idents(seurat_object)
     }
   }
   
   return(seurat_object)
 }
 
+# Run clustering ----------------------------------------------------------
+# Create directory to store PCA stats
+CLUSTERING_OUTPUT_PATH <- file.path(SEURAT_OBJECT_PATH, "clustering_stats")
+dir.create(CLUSTERING_OUTPUT_PATH, showWarnings = FALSE)
 
-# Run clustering
+# Define params
 disps <- seq(0.5, 1.5, 0.5)
 n_features <- c(1000, 2000, 5000)
-k <- seq(10, 50, 5)
+neighbors <- seq(10, 50, 5)
 res <- seq(0.5, 1.2, 0.1)
 
-if (!exists("seurat_object")) {
-  seurat_object <- readRDS(file.path(SEURAT_OBJECT_PATH, "seurat_object.Rds"))
-}
-
 for (disp in disps) {
-  case <- paste("mean.var.plot_disp", disp, sep="")
-  dr <- paste("pca", case, sep="_")
-  out_dir <- file.path(SEURAT_OBJECT_PATH, case)
+  filename <- paste("mean.var.plot_disp", disp, sep="_")
+  dr_name <- paste("pca", filename, sep="_")
+  out_dir <- file.path(SEURAT_OBJECT_PATH, dr_name)
   dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
-  seurat_object <- ClustDr(
+  seurat_object <- Clust_dr(
     seurat_object, 
-    dr = dr, 
-    k = k, 
+    dr = dr_name, 
+    K = neighbors, 
     res = res, 
-    file = file.path(out_dir, "num_PCs.txt")
+    file = file.path(
+      CLUSTERING_OUTPUT_PATH, 
+      paste(dr_name, "num_PCs.txt", sep = "_")
+    )
   )
 }
 
 for (n in n_features) {
-  case <- paste("vst_top", n, sep="")
-  dr <- paste("pca", case, sep="_")
-  out_dir <- file.path(SEURAT_OBJECT_PATH, case)
-  dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
-  seurat_object <- ClustDr(
+  filename <- paste("vst_top", n, sep="")
+  dr_name <- paste("pca", filename, sep="_")
+  out_dir <- file.path(SEURAT_OBJECT_PATH, dr_name)
+  seurat_object <- Clust_dr(
     seurat_object, 
-    dr = dr, 
-    k = k, 
+    dr = dr_name, 
+    K = neighbors, 
     res = res, 
-    file = file.path(out_dir, "num_PCs.txt")
+    file = file.path(
+      CLUSTERING_OUTPUT_PATH, 
+      paste(dr_name, "num_PCs.txt", sep = "_")
+    )
   )
 }
 
--- clustering/1d_seurat_cluster_comp.R
+++ clustering/1d_seurat_cluster_comp.R
@@ -1,7 +1,13 @@
 source(file.path("sc_crohns/clustering", ".Rprofile"))
 
+CLUSTERING_OUTPUT_PATH <- file.path(SEURAT_OBJECT_PATH, "clustering_stats")
 
-# Function to calculate cluster compositions ------------------------------
+if (!exists("seurat_object")) {
+  seurat_object <- readRDS(file.path(SEURAT_OBJECT_PATH, "seurat_object.Rds"))
+}
+
+# Functions to calculate cluster compositions and silhouette scores ------------
+# compute cluster composition
 ClustersComposition <- function(
     seurat_object, 
     out.prefix, 
@@ -57,41 +63,107 @@ ClustersComposition <- function(
   return()
 }
 
-if (!exists("seurat_object")) {
-  seurat_object <- readRDS(file.path(SEURAT_OBJECT_LOC, "seurat_object.Rds"))
+# compute silhouette scores
+SilhouetteSingle <- function(dist, ident) {
+  id <- unique(ident)
+  if (length(id) > 1) { 
+    silh_data <- silhouette(as.numeric(ident), dist)
+    silh_summ <- summary(silh_data)
+    saveRDS(silh_summ, file = file.path(
+      CLUSTERING_OUTPUT_PATH, 
+      "silh_summary.Rds")
+    )
+    ggsave(
+      filename = file.path(CLUSTERING_OUTPUT_PATH, "silh_summary.png"),
+      plot = fviz_silhouette(silh_data), 
+      width = 8, height = 6, dpi = 150
+    )
+  }
+  return()
 }
 
 # get clustering statistics
 disps <- seq(0.5, 1.5, 0.5)
 n_features <- c(1000, 2000, 5000)
-k <- seq(10, 50, 5)
+K <- seq(10, 50, 5)
 res <- seq(0.5, 1.2, 0.1)
 
 # Create list of cases and dimensionality reductions
 cases <- c()
-for (ymin in disps)
-  cases <- c(cases, paste("mean.var.plot_disp", ymin, sep=""))
-for (nfeat in nfeats)
-  cases <- c(cases, paste("vst_top", nfeat, sep=""))
+for (disp in disps) {
+  cases <- c(cases, paste("mean.var.plot_disp", disp, sep="_"))
+}
+for (nfeat in nfeats) {
+  cases <- c(cases, paste("vst_top", nfeat, sep="_"))
+}
 drs <- paste("pca", cases, sep="_")
 
 # Create directories for each dim red
-for (ll in drs) 
+for (ll in drs) {
   dir.create(file.path(ll), showWarnings = FALSE, recursive = TRUE)
-
+}
+  
 # Perform cluster composition computation
 for (dr in drs) {
   print(paste("dr =", dr))
-  for (kk in k) {
+  for (k in K) {
     for (r in res) {
-      cl_ident <- paste("clusters_", dr, "_k", kk, "_res", r, sep="")
+      cl_ident <- paste("clusters", dr, "k", k, "res", r, sep="_")
       out_prefix <- file.path(dr, cl_ident)
       ClustersComposition(
         seurat_object = seurat_object, 
         out.prefix = out_prefix, 
         cl.ident.slot = cl_ident, 
         sample.ident.slot = "orig.ident"
-      ) 
+      )
+      SilhouetteSingle(
+        dist = dist, 
+        ident = c(as.matrix(seurat_object@meta.data[cl_ident])), 
+        out.prefix = out_prefix
+      )
+      
     }
   }
 }
+
+for (kk in k) {
+  for (r in res) {
+    n <- length(silh_data$clus.sizes)
+    avg_silh <- silh_data$si.summary[4]
+    meta <- seurat_object@meta.data[[cl_ident]]
+    avg_umi <- round(mean(seurat_object@meta.data$nCount_RNA))
+    avg_umi_cl <- unlist(lapply(1:n-1, function(x) round(mean(seurat_object@meta.data$nCount_RNA[meta == as.character(x)]))))
+    idx_min_umi <- order(avg_umi_cl)[1]-1
+    cl_size <- sum(meta == as.character(idx_min_umi))
+    v <- c(case, dim, kk, r, n, avg_silh, avg_umi, min(avg_umi_cl), idx_min_umi, cl_size)
+    df <- rbind(df, v)
+  }
+}
+
+df <- data.frame(
+  hvg.set = 1, 
+  PCs = 1, 
+  k = 1, 
+  res = 1, 
+  num.cl = 1, 
+  avg.silh.width = 1, 
+  avg.umi = 1, 
+  min.umi.val = 1, 
+  min.umi.idx = 1, 
+  min.umi.cl.size = 1
+)
+
+df <- data.frame(
+  "hvg.set", 
+  "PCs", 
+  "k", 
+  "res", 
+  "num.cl", 
+  "avg.silh.width", 
+  "avg.umi", 
+  "min.umi.cl.val", 
+  "min.umi.cl.idx", 
+  "min.umi.cl.size"
+)
+
+
