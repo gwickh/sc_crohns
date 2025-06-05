d92394f4955cc359ef78fdd77f7ee4785d16aecc
gwick <Greg.Wickham@earlham.ac.uk>
2025-06-05 09:19:43 +0100

    move function to script

--- clustering/1c_seurat_clustering.R
+++ clustering/1c_seurat_clustering.R
@@ -1,26 +1,65 @@
 source(file.path("sc_crohns/clustering", ".Rprofile"))
 
-# Run clustering
-clustering_params <- list(
-  disps = 0.5,
-  nfeats = 5000,
-  k = 30,
-  res = seq(0.5, 1.2, 0.1)
-)
+# Function to identify number of informative PCs --------------------------
+# based on standard deviation cutoff and cluster with Louvain at varying on
+# resolution and kNN
+Clust_dr <- function(
+    seurat_object, 
+    dr, 
+    pcs = 50, 
+    pval = 1e-05, 
+    non.rand.sd.frac = 0.5, 
+    k, 
+    res, 
+    file
+) {
+  # Compute minimum std a PC must have to be considered “non-random” assuming
+  # last 10 PCs approximate random noise to exclude SVD artifacts
+  min_stdev <- (1+non.rand.sd.frac)*mean(seurat_object@reductions[[dr]]@stdev[(pcs-10):pcs])
+  
+  # Loops through PCs to determine nPCs, the number of informative PCs that 
+  # explains > 0.05 variance with respect to the next
+  for (nPCs in 0:(length(seurat_object@reductions[[dr]]@stdev)-1)) {
+    if (seurat_object@reductions[[dr]]@stdev[nPCs+1] < min_stdev) 
+      break
+  }
+  write(nPCs, file = file)
+  
+  # For each k build a NN graph using the top nPCs components
+  for (kk in k) {
+    seurat_object <- FindNeighbors(
+      seurat_object, 
+      k.param = kk, 
+      reduction = dr, 
+      dims = 1:nPCs
+    )
+    # For each resolution, run Louvain clustering
+    for (r in res) {
+      cl_ident <- paste("clusters_", dr, "_k", kk, "_res", r, sep="")
+      seurat_object <- FindClusters(seurat_object, resolution = r)
+      names(seurat_object@meta.data)[names(seurat_object@meta.data) == "seurat_clusters"] <- cl_ident
+      seurat_object@active.ident <- as.factor(seurat_object$orig.ident)
+    }
+  }
+  
+  return(seurat_object)
+}
 
-disps <- clustering_params$disps
-nfeats <- clustering_params$nfeats
-k <- clustering_params$k
-res <- clustering_params$res
+
+# Run clustering
+disps <- seq(0.5, 1.5, 0.5)
+n_features <- c(1000, 2000, 5000)
+k <- seq(10, 50, 5)
+res <- seq(0.5, 1.2, 0.1)
 
 if (!exists("seurat_object")) {
-  seurat_object <- readRDS(file.path(SEURAT_OBJECT_LOC, "seurat_object.Rds"))
+  seurat_object <- readRDS(file.path(SEURAT_OBJECT_PATH, "seurat_object.Rds"))
 }
 
-for (ymin in disps) {
-  case <- paste("mean.var.plot_disp", ymin, sep="")
+for (disp in disps) {
+  case <- paste("mean.var.plot_disp", disp, sep="")
   dr <- paste("pca", case, sep="_")
-  out_dir <- file.path(OUTDIR, case)
+  out_dir <- file.path(SEURAT_OBJECT_PATH, case)
   dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
   seurat_object <- ClustDr(
     seurat_object, 
@@ -31,10 +70,10 @@ for (ymin in disps) {
   )
 }
 
-for (nfeat in nfeats) {
-  case <- paste("vst_top", nfeat, sep="")
+for (n in n_features) {
+  case <- paste("vst_top", n, sep="")
   dr <- paste("pca", case, sep="_")
-  out_dir <- file.path(OUTDIR, case)
+  out_dir <- file.path(SEURAT_OBJECT_PATH, case)
   dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
   seurat_object <- ClustDr(
     seurat_object, 
@@ -45,4 +84,4 @@ for (nfeat in nfeats) {
   )
 }
 
-saveRDS(seurat_object, file = file.path(OUTDIR, "seurat_object.Rds"))
+saveRDS(seurat_object, file = file.path(SEURAT_OBJECT_PATH, "seurat_object.Rds"))
--- clustering/1d_seurat_cluster_comp.R
+++ clustering/1d_seurat_cluster_comp.R
@@ -1,21 +1,71 @@
 source(file.path("sc_crohns/clustering", ".Rprofile"))
 
-# compute cluster composition 
-clustering_params <- list(
-  disps = 0.5,
-  nfeats = 5000,
-  k = 30,
-  res = c(0.5,0.6,0.7,0.8,0.9,1,1.1,1.2)
-)
 
-disps <- clustering_params$disps
-nfeats <- clustering_params$nfeats
-k <- clustering_params$k
-res <- clustering_params$res
+# Function to calculate cluster compositions ------------------------------
+ClustersComposition <- function(
+    seurat_object, 
+    out.prefix, 
+    cl.ident.slot,
+    sample.ident.slot = "orig.ident",
+    width = 6, 
+    height = 3
+) {
+  if (!all(c(cl.ident.slot, sample.ident.slot) %in% colnames(seurat_object@meta.data))) {
+    stop("One or both of the specified metadata slots do not exist.")
+  }
+  
+  sample_ident <- seurat_object@meta.data[[sample.ident.slot]]
+  cl_ident <- seurat_object@meta.data[[cl.ident.slot]]
+  
+  # Count cells per (sample, cluster)
+  df <- as.data.frame(table(sample = sample_ident, cluster = cl_ident))
+  
+  # Ensure numeric values and correct factor levels
+  df$num <- as.numeric(df$Freq)
+  df$cluster <- factor(df$cluster, levels = sort(unique(cl_ident)))
+  df$sample <- factor(df$sample, levels = rev(sort(unique(sample_ident))))
+  df$Freq <- NULL  # remove redundant column
+  
+  # Save raw counts
+  write.table(df, file = paste0(out.prefix, ".tsv"), row.names = FALSE, sep = "\t", quote = FALSE)
+  
+  # Plot function
+  plot_bar <- function(data, x, y, fill, filename, norm = FALSE) {
+    g <- ggplot(data, aes_string(x = x, y = y, fill = fill)) + 
+      theme_minimal() + 
+      coord_flip() + 
+      xlab("") + 
+      theme(axis.text.y = element_text(size = 15))
+    
+    if (norm) {
+      g <- g + geom_bar(stat = "identity", position = "fill") + ylab("fraction of cells")
+    } else {
+      g <- g + geom_bar(stat = "identity") + ylab("number of cells")
+    }
+    
+    pdf(filename, width = width, height = height)
+    print(g)
+    dev.off()
+  }
+  
+  # Generate plots
+  plot_bar(df, "sample", "num", "cluster", paste0(out.prefix, "_SbyC_abs.pdf"), norm = FALSE)
+  plot_bar(df, "sample", "num", "cluster", paste0(out.prefix, "_SbyC_norm.pdf"), norm = TRUE)
+  plot_bar(df, "cluster", "num", "sample", paste0(out.prefix, "_CbyS_abs.pdf"), norm = FALSE)
+  plot_bar(df, "cluster", "num", "sample", paste0(out.prefix, "_CbyS_norm.pdf"), norm = TRUE)
+  
+  return()
+}
 
 if (!exists("seurat_object")) {
   seurat_object <- readRDS(file.path(SEURAT_OBJECT_LOC, "seurat_object.Rds"))
 }
+
+# get clustering statistics
+disps <- seq(0.5, 1.5, 0.5)
+n_features <- c(1000, 2000, 5000)
+k <- seq(10, 50, 5)
+res <- seq(0.5, 1.2, 0.1)
 
 # Create list of cases and dimensionality reductions
 cases <- c()
