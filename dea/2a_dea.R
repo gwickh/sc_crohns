60b88da56e13262a093b45a4089e8516560ec1bd
gwick <Greg.Wickham@earlham.ac.uk>
2025-06-10 14:55:15 +0100

    call functions

--- dea/2a_dea.R
+++ dea/2a_dea.R
@@ -1,7 +1,3 @@
-OBJECT <- "seurat_object.rds"
-OUT_DIR <- "dea_output"
-CLUSTER_ID <- "seurat_clusters"
-TEST <- "wilcox"
 NO_PAIRS <- FALSE
 MIN_PERC <- 0.1
 LOGFC <- 0.25
@@ -12,59 +8,70 @@ TOP_DEG <- 50
 GENES_EXCLUDE <- NULL
 CORES <- 8
 
-DEA_OUT_DIR <- file.path(OUT_DIR, TEST)
+DEA_OUT_DIR <- file.path(SEURAT_OBJECT_PATH, "DEA_output")
 dir.create(DEA_OUT_DIR, showWarnings = FALSE)
 
 if (!exists("seurat_object")) {
   seurat_object <- readRDS(file.path(SEURAT_OBJECT_LOC, "seurat_object.Rds"))
 }
 
-Idents(seurat_object) <- seurat_object@meta.data[[CLUSTER_ID]]
+source(file.path("sc_crohns/clustering", ".Rprofile"))
+source(file.path("sc_crohns/dea", "2_dea_functions.R"))
 
-genes <- rownames(GetAssayData(object, slot = "counts"))
+Idents(seurat_object) <- seurat_object@meta.data[["seurat_clusters"]]
+
+genes <- rownames(LayerData(seurat_object, layer = "counts"))
 if (!is.null(GENES_EXCLUDE)) {
   genes <- genes[!(genes %in% GENES_EXCLUDE)]
 }
 
 if (!NO_PAIRS) {
-  ClusterGeneMarkersByPairs(object = object,
-                            out.dir = DEA_OUT_DIR,
-                            id = CL_MODE,
-                            test.use = TEST,
-                            min.pct = MIN_PERC,
-                            logFC = LOGFC,
-                            latent.vars = LATENT_VARS)
+  ClusterGeneMarkersByPairs(
+    object = seurat_object,
+    out.dir = DEA_OUT_DIR,
+    id = CL_MODE,
+    test.use = TEST,
+    min.pct = MIN_PERC,
+    logFC = LOGFC,
+    latent.vars = LATENT_VARS
+  )
 }
 
-ClusterGeneMarkersVsAll(object = object,
-                        out.dir = DEA_OUT_DIR,
-                        id = CL_MODE,
-                        test.use = TEST,
-                        min.pct = MIN_PERC,
-                        logFC = LOGFC,
-                        latent.vars = LATENT_VARS)
+ClusterGeneMarkersVsAll(
+  object = seurat_object,
+  out.dir = DEA_OUT_DIR,
+  id = CL_MODE,
+  test.use = TEST,
+  min.pct = MIN_PERC,
+  logFC = LOGFC,
+  latent.vars = LATENT_VARS
+)
 
 # ==== Filtering + Plots ====
 if (!NO_PAIRS) {
-  FilterClusterGeneMarkersPairs(object = object,
-                                out.dir = DEA_OUT_DIR,
-                                id = CL_MODE,
-                                test.use = TEST,
-                                logFC.filt = LOGFC_FILT,
-                                adjpval.filt = ADJ_PVAL_FILT,
-                                num = TOP_DEG,
-                                genes.use = genes,
-                                heatmap = TRUE,
-                                volcano = TRUE)
+  FilterClusterGeneMarkersPairs(
+    object = seurat_object,
+    out.dir = DEA_OUT_DIR,
+    id = CL_MODE,
+    test.use = TEST,
+    logFC.filt = LOGFC_FILT,
+    adjpval.filt = ADJ_PVAL_FILT,
+    num = TOP_DEG,
+    genes.use = genes,
+    heatmap = TRUE,
+    volcano = TRUE
+  )
 }
 
-FilterClusterGeneMarkersAll(object = object,
-                            out.dir = DEA_OUT_DIR,
-                            id = CL_MODE,
-                            test.use = TEST,
-                            logFC.filt = LOGFC_FILT,
-                            adjpval.filt = ADJ_PVAL_FILT,
-                            num = TOP_DEG,
-                            genes.use = genes,
-                            heatmap = TRUE,
-                            volcano = TRUE)
+FilterClusterGeneMarkersAll(
+  object = seurat_object,
+  out.dir = DEA_OUT_DIR,
+  id = CL_MODE,
+  test.use = TEST,
+  logFC.filt = LOGFC_FILT,
+  adjpval.filt = ADJ_PVAL_FILT,
+  num = TOP_DEG,
+  genes.use = genes,
+  heatmap = TRUE,
+  volcano = TRUE
+)
