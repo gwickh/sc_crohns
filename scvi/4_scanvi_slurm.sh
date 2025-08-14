44ac67f50f8e4894bc552d1a7d30757d7e78074d
gwick <Greg.Wickham@earlham.ac.uk>
2025-08-14 13:49:39 +0100

    create scanvi scripts

---
+++ scvi/4_scanvi_slurm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# SLURM directives
+#SBATCH --job-name=scanvi_cluster
+#SBATCH --output=4_scanvi_%j.SLURM.stdout
+#SBATCH --error=4_scanvi_%j.SLURM.stderr
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=256G
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=yep25yan@nbi.ac.uk
+#SBATCH --partition=ei-long
+
+# Arguments
+SCRIPT_DIR="$1"
+OUT_DIR="$2"
+
+# Create output directory if it doesn't exist
+mkdir -p "${OUT_DIR}"
+
+source ~/.bashrc
+conda activate python_scripts
+scripts=(
+    "4c_scanvi.py"
+)
+
+for script in "${scripts[@]}"; do
+    echo "Running ${script}"
+    python "${SCRIPT_DIR}/${script}" \
+            1> "${OUT_DIR}/${script}.stdout" \
+            2> "${OUT_DIR}/${script}.stderr"
+    echo "${script} completed"
+done
+
+exit 0
