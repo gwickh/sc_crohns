de57435aeedacf7a6ad4b1ae4caa53d8cac26e00
gwick <Greg.Wickham@earlham.ac.uk>
2025-11-19 17:03:15 +0000

    perform platform-aware adaptive filtering

--- scanpy_clustering/1a_scanpy_load_matrices.py
+++ scanpy_clustering/1a_scanpy_load_matrices.py
@@ -45,28 +45,38 @@ for path, sample in zip(matrix_paths, sample_names):
     adata_list.append(adata)
 
 # Compute per-sample QC metrics and mitochondrial and ribosomal genes
-qc_list = []
-for sample in adata_list:
-    # Identify mitochondrial and ribosomal genes
-    sample.var["ribo"] = sample.var["gene_name"].str.startswith(("RPS", "RPL"))
-    if sample.var["ribo"].sum() == 0:
-        raise ValueError(f"No ribosomal genes found in {sample.obs['sample_id'][0]}")
-    
-    sample.var["mt"] = sample.var["gene_name"].str.startswith("MT-")
-    if sample.var["mt"].sum() == 0:
-        raise ValueError(f"No mitochondrial genes found in {sample.obs['sample_id'][0]}")
+def compute_qc_metrics(adata_list) -> list:
+    """
+    Compute QC metrics for each sample in adata_list.
+    """
+    for sample in adata_list:
+        
+        # Remove empty droplets
+        sc.pp.filter_cells(sample, min_counts=1000)
 
-    # Calculate QC metrics
-    sc.pp.calculate_qc_metrics(
-        sample,
-        qc_vars=["mt", "ribo"],
-        inplace=True,
-        log1p=True
-    )
+        # Identify mitochondrial and ribosomal genes
+        sample.var["ribo"] = sample.var["gene_name"].str.startswith(("RPS", "RPL"))
+        if sample.var["ribo"].sum() == 0:
+            raise ValueError(f"No ribosomal genes found in {sample.obs['sample_id'][0]}")
+        
+        sample.var["mt"] = sample.var["gene_name"].str.startswith("MT-")
+        if sample.var["mt"].sum() == 0:
+            raise ValueError(f"No mitochondrial genes found in {sample.obs['sample_id'][0]}")
 
-# Visualize QC metrics across samples
+        # Calculate QC metrics
+        sc.pp.calculate_qc_metrics(
+            sample,
+            qc_vars=["mt", "ribo"],
+            inplace=True,
+            log1p=True
+        )
+    return adata_list
+
+# Create dataframe of QC metrics from anndata object list
 def obtain_metrics(adata_list, qc_dict) -> pd.DataFrame:
-    """Return long-format QC metrics for all samples."""
+    """
+    Return long-format QC metrics for all samples.
+    """
     
     rows = []
 
@@ -88,6 +98,7 @@ def obtain_metrics(adata_list, qc_dict) -> pd.DataFrame:
 
     return pd.concat(rows, ignore_index=True)
 
+# Plot QC metrics
 def qc_plots(metrics_df, qc_dict, stage="") -> None:
     """
     Plot QC metrics across samples
@@ -156,6 +167,89 @@ def qc_plots(metrics_df, qc_dict, stage="") -> None:
         bbox_inches="tight"
     )
 
+# Utility to create MAD mask for a given metric
+def create_mad_mask(adata_list, sample, metric, nmads=3) -> np.ndarray:
+
+    platform = sample.obs["platform"].iloc[0]
+    platform_values = []
+
+    for s in adata_list:
+        if s.obs["platform"].iloc[0] == platform:
+            platform_values.append(s.obs[metric].values)
+
+    platform_values = np.concatenate(platform_values)
+    sample_values = sample.obs[metric].values
+
+    med = np.median(platform_values)
+    mad = median_abs_deviation(platform_values, scale=0.6745)
+
+    # guard for collapse of bounds and dropping all cells when mad is near zero
+    if mad < 1e-8:
+        mask = np.ones(sample.n_obs, dtype=bool)    
+    else:
+        mask = (sample_values > med - nmads * mad) & (sample_values < med + nmads * mad)
+    return mask
+
+# Apply MAD filtering across samples
+def mad_filter(adata_list, qc_dict, nmads=3) -> list:
+    """
+    Compute MAD boolean masks and filter per sample
+    """
+    adata_list_filtered = []
+
+    for sample in adata_list:
+
+        combined_mask = np.ones(sample.n_obs, dtype=bool)   # init combined mask to all True
+
+        for label, metric in qc_dict.items():
+            mask = create_mad_mask(adata_list, sample, metric)
+            combined_mask &= mask                            # logical AND to combine masks across metrics 
+
+        adata_filtered = sample[combined_mask, :].copy()
+        adata_list_filtered.append(adata_filtered)
+
+    return adata_list_filtered
+
+# Get QC stats and save to CSV
+def obtain_qc_stats(adata_list, adata_list_filtered, qc_dict, outpath) -> None:
+    """
+    Save QC stats to CSV.
+    """
+    adata_stats = []
+
+    for sample, filtered in zip(adata_list, adata_list_filtered):
+
+        sample_id = sample.obs["sample_id"].iloc[0]
+        diagnosis = "Crohn's Disease" if "Crohns" in sample_id else "Normal"
+
+        row = {
+            "Sample ID": sample_id,
+            "Diagnosis": diagnosis,
+            "Initial number of cells": sample.n_obs,
+            "Final number of cells": filtered.n_obs,
+            "Proportion retained": filtered.n_obs / sample.n_obs,
+        }
+        for label, metric in qc_dict.items():
+
+            values = sample.obs[metric].values
+            med = np.median(values)
+            mad = median_abs_deviation(values, scale=0.6745)
+            mask = create_mad_mask(adata_list, sample, metric)
+
+            row.update({
+                f"{label} median": med,
+                f"{label} MAD": mad,
+                f"{label} passed": mask.sum(),
+                f"{label} passed fraction": mask.sum() / sample.n_obs,
+            })
+
+        adata_stats.append(row)
+
+    adata_df = pd.DataFrame(adata_stats)
+    adata_df.to_csv(os.path.join(outpath, "qc_stats_mad_filtering.csv"), index=False)
+
+
+# Run preprocessing steps
 qc_dict = {
         "Number of genes": "n_genes_by_counts", 
         "Log10 total read count": "log1p_total_counts",
@@ -163,43 +257,19 @@ qc_dict = {
         "Percent ribosomal reads": "pct_counts_ribo",
     }
 
+compute_qc_metrics(adata_list)
+
 metrics_df = obtain_metrics(adata_list, qc_dict)
-qc_plots(metrics_df, qc_dict, stage="raw")
+qc_plots(metrics_df, qc_dict, stage="Raw")
+
+adata_list_filtered = mad_filter(adata_list, qc_dict)
+
+metrics_df_filtered = obtain_metrics(adata_list_filtered, qc_dict)
+qc_plots(metrics_df_filtered, qc_dict, stage="Filtered")
+
+obtain_qc_stats(adata_list, adata_list_filtered, qc_dict, OUTPATH)
 
 
-# Filter low-quality cells by median absolute deviation
-def mad_filter(adata_list, qc_dict, nmads=3):
-    """
-    Compute MAD boolean masks and filter per sample
-    """
-    results = {}
-
-    for label, metric in qc_dict.items():
-        metric_masks = {}
-
-        for sample in adata_list:
-            values = sample.obs[metric].values
-
-            med = np.median(values)
-            mad = median_abs_deviation(values, scale=0.6745)
-
-            mask = (values > med - nmads * mad) & (values < med + nmads * mad)
-
-            sample_id = sample.obs["sample_id"].iloc[0]
-            metric_masks[sample_id] = mask
-
-        results[metric] = metric_masks
-
-    return results
-
-adata_list = mad_filter(adata_list, qc_dict)
-
-# adata = adata[(adata.obs.n_genes_by_counts > 500) & 
-#               (adata.obs.n_genes_by_counts < 6000) & 
-#               (adata.obs.pct_counts_mt < 10), :]
-
-# sc.pp.filter_cells(adata, min_genes=100)
-# sc.pp.filter_genes(adata, min_cells=3)
 
 # Normalize
 # sc.pp.normalize_total(adata, target_sum=1e4)
