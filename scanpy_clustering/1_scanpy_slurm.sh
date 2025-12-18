4c8b27b4c052bd58e670ce3e5245cefae313b741
gwick <Greg.Wickham@earlham.ac.uk>
2025-12-18 15:59:33 +0000

    comment out run scripts

--- scanpy_clustering/1_scanpy_slurm.sh
+++ scanpy_clustering/1_scanpy_slurm.sh
@@ -4,11 +4,13 @@
 #SBATCH --job-name=scanpy_cluster
 #SBATCH --output=1_scanpy_%j.SLURM.stdout
 #SBATCH --error=1_scanpy_%j.SLURM.stderr
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=128G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=256G
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=yep25yan@nbi.ac.uk
-#SBATCH --partition=ei-short
+#SBATCH --partition=ei-medium
+
+# usage: sbatch sc_crohns/scanpy_clustering/1_scanpy_slurm.sh sc_crohns/scanpy_clustering/ project-area/data/crohns_scrnaseq/10c_14n_analysis/scanpy/
 
 # Arguments
 SCRIPT_DIR="$1"
@@ -20,9 +22,12 @@ mkdir -p "${OUT_DIR}"
 source ~/.bashrc
 conda activate python_scripts
 scripts=(
-    "1a_scanpy_load_matrices.py"
-    "1b_scanpy_PCA.py"
+    # "1a_scanpy_load_matrices.py"
+    # "1b_scanpy_PCA.py"
+    # "1c_scanpy_clustering.py"
+    "1e_scanpy_UMAP.py"
 )
+
 
 for script in "${scripts[@]}"; do
     echo "Running ${script}"
