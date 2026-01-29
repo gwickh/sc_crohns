f30b54b11a17a946e05fa8bca57e88f10f1c1b6c
gwick <Greg.Wickham@earlham.ac.uk>
2026-01-29 00:54:18 +0000

    perform hyperparameter search for scvi

--- scvi/4_scanvi_slurm.sh
+++ scvi/4_scanvi_slurm.sh
@@ -4,13 +4,13 @@
 #SBATCH --job-name=scanvi_cluster
 #SBATCH --output=4_scanvi_%j.SLURM.stdout
 #SBATCH --error=4_scanvi_%j.SLURM.stderr
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=128G
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=256G
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=yep25yan@nbi.ac.uk
-#SBATCH --partition=ei-short
+#SBATCH --partition=ei-long
 
-# Usage: sbatch sc_crohns/scvi/4_scanvi_slurm.sh sc_crohns/scvi/ project-area/data/crohns_scrnaseq/scvi_tools_output/
+# Usage: sbatch sc_crohns/scvi/4_scanvi_slurm.sh sc_crohns/scvi/ project-area/data/crohns_scrnaseq/10c_14n_analysis/scvi_tools_output/
 
 # Arguments
 SCRIPT_DIR="$1"
@@ -22,12 +22,12 @@ mkdir -p "${OUT_DIR}"
 source ~/.bashrc
 conda activate python_scripts
 scripts=(
-    # "4a_scvi_ref.py"
+    "4a_scvi_ref.py"
     # "4b_scanvi_ref.py"
     # "4c_scanvi_query.py"
     # "4d_scanpy_clustering.py"
     # "4e_celltype_similarity_matrix.py"
-    "4f_curated_celltype_clustering.py"
+    # "4f_curated_celltype_clustering.py"
 )
 
 for script in "${scripts[@]}"; do
--- scvi/4a_scvi_ref.py
+++ scvi/4a_scvi_ref.py
@@ -1,22 +1,22 @@
 import os
+from pathlib import Path
 
-import ray
 import scanpy as sc
 from ray import tune
-from ray.tune.schedulers import ASHAScheduler
-from scvi_colab import install
 
 import scvi
 from scvi import autotune
 
-install()
-
 SCVI_PATH = "project-area/data/crohns_scrnaseq/10c_14n_analysis/scvi_tools_output"
 os.makedirs(SCVI_PATH, exist_ok=True)
 
+LOG_PATH = Path(
+    "project-area/data/crohns_scrnaseq/10c_14n_analysis/scvi_tools_output/scvi_autotune_log"
+).resolve()
+LOG_PATH.mkdir(parents=True, exist_ok=True)
+
 GCA_OBJ_PATH = os.path.join(SCVI_PATH, "Full_obj_raw_counts_nosoupx_v2.h5ad")
 REF_OBJ_PATH = os.path.join(SCVI_PATH, "obj_healthy_adult_pediatric_TIL.h5ad")
-LOG_PATH = os.path.join(SCVI_PATH, "scvi_autotune.log")
 
 if os.path.exists(REF_OBJ_PATH):
     print(f"Loading existing reference object {REF_OBJ_PATH}")
@@ -39,65 +39,66 @@ sc.pp.highly_variable_genes(
     adata,
     flavor="seurat_v3",
     n_top_genes=5000,
-    layer="counts",
-    batch_key="sample_id",
+    batch_key="batch",
     subset=True,
 )
 
 # setup scvi model class
 model_cls = scvi.model.SCVI
-model_cls.setup_anndata(adata)
-
-scvi_tuner = autotune.ModelTuner(model_cls)
-
-search_space = {
-    # VAE architecture
-    "n_hidden": tune.choice([64, 128, 256]),
-    "n_layers": tune.choice([1, 2, 3]),
-    "n_latent": tune.choice([10, 20, 30]),
-    "dropout_rate": tune.choice([0.05, 0.1, 0.2]),
-    # Training plan
-    "lr": tune.loguniform(1e-4, 1e-2),
-    "weight_decay": tune.loguniform(1e-8, 1e-4),
-    # set hyperparameter values that differ from defaults
-    "n_epochs_kl_warmup": 25,
-    "use_layer_norm": "both",
-    "use_batch_norm": "none",
-    "encode_covariates": True,
-    "reduce_lr_on_plateau": True,
-}
-
-train_kwargs = {
-    "check_val_every_n_epoch": 1,
-    "early_stopping": True,
-    "early_stopping_monitor": "validation_loss",
-    "early_stopping_patience": 20,
-    "early_stopping_min_delta": 0.0,
-}
-
-# define scheduler for hyperparameter optimization
-scheduler = ASHAScheduler(
-    time_attr="training_iteration",
-    metric="validation_loss",
-    mode="min",
-    max_t=250,
-    grace_period=25,
-    reduction_factor=2,
-)
-
-ray.init(log_to_driver=False)
-
-results = scvi_tuner.fit(
+model_cls.setup_anndata(
     adata,
-    metric="validation_loss",
-    search_space=search_space,
-    scheduler=scheduler,
-    train_kwargs=train_kwargs,
-    num_samples=50,
-    max_epochs=250,
-    logging_dir=LOG_PATH,
+    batch_key="batch",
+    continuous_covariate_keys=["pct_counts_mt"],
+    categorical_covariate_keys=["10X"],
 )
 
+# define hyperparameters
+search_space = {
+    "model_params": {
+        "n_hidden": tune.choice([64, 128, 256]),
+        "n_layers": tune.choice([1, 2, 3]),
+        "n_latent": tune.choice([10, 20, 30]),
+        "dropout_rate": tune.choice([0.05, 0.1, 0.2]),
+    },
+    "train_params": {
+        "max_epochs": 250,
+        "check_val_every_n_epoch": 1,
+        "early_stopping": True,
+        "early_stopping_monitor": "validation_loss",
+        "early_stopping_patience": 20,
+        "early_stopping_min_delta": 0.0,
+        "plan_kwargs": {
+            "lr": tune.loguniform(3e-4, 3e-3),
+            "weight_decay": tune.loguniform(1e-8, 1e-4),
+            "eps": 1e-2,
+            "n_epochs_kl_warmup": 20,
+        },
+    },
+}
+
+scheduler_kwargs = {
+    "max_t": 250,
+    "grace_period": 25,
+    "reduction_factor": 2,
+}
+
+# train models
+results = autotune.run_autotune(
+    model_cls,
+    adata,
+    metrics="validation_loss",
+    mode="min",
+    search_space=search_space,
+    scheduler="asha",
+    scheduler_kwargs=scheduler_kwargs,
+    num_samples=50,
+    logging_dir=str(LOG_PATH),
+)
+
+print(results)
+
+df = results.get_dataframe()
+df.to_csv(os.path.join(LOG_PATH, "scvi_autotune_results.csv"), index=False)
 
 # scvi.model.SCVI.setup_anndata(ref_adata, layer="counts", batch_key="sample_id")
 
